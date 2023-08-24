5177e70f033a3df36f9e2e6d02cfdf39958dd6c2
chadrakh <chadrak@outlook.com>
2023-08-24 21:44:34 +0100

    Feature/Add InterfaceMessages class for user interaction

---
+++ src/InterfaceMessages.java
@@ -0,0 +1,18 @@
+public class InterfaceMessages {
+
+    public void actions() {
+
+    }
+
+    public void loginSuccess() {
+        System.out.println("\nYou have successfully logged in.");
+    }
+
+    public void loginFail(int remainingAttempts) {
+        System.out.printf("\nIncorrect username or password. \n%d attempts remaining.\n\n", remainingAttempts);
+    }
+
+    public void maxAttemptsWarning() {
+        System.out.println("*Maximum attempts reached (3).* \nYou have been locked out of the system.");
+    }
+}
