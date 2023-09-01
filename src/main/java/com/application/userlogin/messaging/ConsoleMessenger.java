bf8e56b3fe95d847efd874c660413f3de7e06020
chadrakh <chadrak@outlook.com>
2023-09-01 22:31:12 +0100

    Feat: Add ConsoleMessenger class for user interaction

--- src/main/java/com/application/userlogin/messaging/ConsoleMessenger.java
+++ src/main/java/com/application/userlogin/messaging/ConsoleMessenger.java
@@ -1,20 +1,4 @@
 package com.application.userlogin.messaging;
 
-public class InterfaceMessages {
-
-    public void actions() {
-
-    }
-
-    public void loginSuccess() {
-        System.out.println("\nYou have successfully logged in.");
-    }
-
-    public void loginFail(int remainingAttempts) {
-        System.out.printf("\nIncorrect username or password. \n%d attempts remaining.\n\n", remainingAttempts);
-    }
-
-    public void maxAttemptsWarning() {
-        System.out.println("*Maximum attempts reached (3).* \nYou have been locked out of the system.");
-    }
+public class ConsoleMessenger {
 }
