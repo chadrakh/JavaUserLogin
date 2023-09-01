0df60ddf9fc4914fe2272e8e3b160ccde16f68f0
chadrakh <chadrak@outlook.com>
2023-09-01 22:54:54 +0100

    Feat: Add UserRepositoryTest test cases

--- src/test/java/com/application/userlogin/repository/UserRepositoryTest.java
+++ src/test/java/com/application/userlogin/repository/UserRepositoryTest.java
@@ -1,2 +1,48 @@
-package com.application.userlogin.repository;public class UserRepositoryTest {
+package com.application.userlogin.repository;
+
+import org.junit.jupiter.api.Disabled;
+
+public class UserRepositoryTest {
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void checkValidUserIsFoundByUsername() {
+
+    }
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void checkInvalidUserIsNotFoundByUsername() {
+
+    }
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void checkValidUserIsFoundById() {
+
+    }
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void checkInvalidUserIsNotFoundById() {
+
+    }
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void checkUserIsAddedToDb() {
+
+    }
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void checkUserIsRemovedFromDb() {
+
+    }
+
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void checkUserTypeIsUpdated() {
+
+    }
 }
