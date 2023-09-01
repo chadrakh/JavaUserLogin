c735f793646fac647c85b5218d39ad8d327820e7
chadrakh <chadrak@outlook.com>
2023-09-01 21:54:59 +0100

    Feat: Add UserHandlerTest and ConsoleMessengerTest classes

--- src/main/test/com/authentication/userlogin/MyTest.java
+++
@@ -1,2 +0,0 @@
-package com.authentication.userlogin;public class MyTest {
-}
---
+++ src/main/test/com/authentication/userlogin/UserHandlerTest.java
@@ -0,0 +1,11 @@
+package com.authentication.userlogin;
+
+import org.junit.jupiter.api.Disabled;
+
+public class UserHandlerTest {
+    @Disabled
+    @org.junit.jupiter.api.Test
+    public void check() {
+
+    }
+}
